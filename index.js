211e901972cece6a93778b11fc6b35c4280e0ccb
DevanshArora312 <devansharora312@gmail.com>
2024-03-04 02:25:07 +0530

    rebase 2

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ const fs = require("fs");
 app.listen(process.env.PORT || 1337, async () => {
     console.log(`webhook is listening at "http://localhost:1337"` );
     
-    const finalData = await checkPdfs();
+    cron.schedule('0 * * * *', async () => {
+      const finalData = await checkPdfs();
       console.log(finalData)
       for(let i=0;i<finalData.length;i++){
         await send(finalData[i]);
       }
+    }, {
+      scheduled: true,
+      timezone: "Asia/Kolkata"
+    });
+    
 });
 app.use(express.json())
 
